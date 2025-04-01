6fed0e3aef11756a51cbee0a402050c6e94436d1
MaxS-9779 <maks_sazanov1998@mail.ru>
2025-04-01 20:15:18 +0300

    Lombok didn't survive

--- src/main/java/org/example/TaskEntity.java
+++ src/main/java/org/example/TaskEntity.java
@@ -1,14 +1,8 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
+
 public class TaskEntity {
     public int id;
     public String name;
